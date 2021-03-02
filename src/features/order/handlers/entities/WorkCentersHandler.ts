5ccb4209f74a2fe3881f6969c9a3e291c6ca6f22
Jorge Grau <jgrau@seidor.es>
2021-03-02 14:37:50 +0100

    SFC-33 refactor para ajustarse a SAP CAP framework
    - POST asíncrono no permite trabajar con cls-hook como estaba previsto
    - CDS con tx trabaja con la transacción actual. Utilizar CDS.transaction para empezar o unirse a una tx iniciada
    - Los @Handler deben iniciar el contexto

--- src/features/order/handlers/entities/WorkCentersHandler.ts
+++ src/features/order/handlers/entities/WorkCentersHandler.ts
@@ -1,14 +1,31 @@
-import { OrderService } from "@Shared/Contract";
-import { IUser } from "@Shared/IUser";
-import { BeforeCreate, Entities, Handler, User } from "cds-routing-handlers";
+import { OrderService } from '@Shared/Contract';
+import { BaseHandler } from '@Core/BaseHandler';
+import { IUser } from '@Shared/IUser';
+import { BeforeCreate, Handler, User, Data, Req, Entities, AfterCreate } from 'cds-routing-handlers';
+import { DIContainer } from '@Root/application/DIContainer';
+import { Request } from '@sap/cds/apis/services';
+import { GetPersonsUseCase } from '@Root/features/tests/usecases/GetPersonUseCase';
+
 
 @Handler(OrderService.SanitizedEntity.WorkCenters)
-export class WorkCentersHandler{
+export class WorkCentersHandler extends BaseHandler {
+  @BeforeCreate()
+  async before(@Req() req: Request, @Data() workcenter: OrderService.IWorkCenters, @User() incommingUser: Promise<IUser>) {
+    return this.runWithContext(req, async () => {
+      this.logger.i(WorkCentersHandler.name, () => '@BeforeCreate name: start');
+      const useCase: GetPersonsUseCase = DIContainer.get(GetPersonsUseCase);
+      const result = await useCase.execute({});
+      if (result.isRight()) {
+        this.logger.v(WorkCentersHandler.name, () => JSON.stringify(result.rightValue()));
+      } else return workcenter;
+      this.logger.i(WorkCentersHandler.name, () => '@BeforeCreate name: end');
+    });
+  }
 
-
-    @BeforeCreate()
-    async name(@Entities() persons: OrderService.IWorkCenters, @User() incommingUser: Promise<IUser>) {
-        console.log("test");
-    }
-
+  @AfterCreate()
+  async after(@Req() req: Request, @Entities() workcenters: OrderService.IWorkCenters, @User() incommingUser: Promise<IUser>) {
+    return this.runWithContext(req, async () => {
+      this.logger.i(WorkCentersHandler.name, () => '@AfterCreate name: start');
+    });
+  }
 }
