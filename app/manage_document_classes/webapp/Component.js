5fbaad6bb61b0147e421ee9e6cf0780b83e21511
cmenendezh <carlos.menendez@seidor.es>
2021-03-10 16:32:00 +0100

    SFC-69 app commit init

---
+++ app/manage_document_classes/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("com.seidor.sfc.apps.manage.managedocumentclasses.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
