1e4c481c1988f26102b029a8dd8e896c29b4d96c
Eloy Asensio <easensio@seidor.es>
2021-03-17 09:30:56 +0100

    SFC-181 Eliminar clase QueryObject - Eliminar clase QueryObject que no se utiliza

--- src/core/QueryObject.ts
+++
@@ -1,9 +0,0 @@
-export class QueryObject {
-	public readonly filter?: Map<string, any>;
-	public readonly limit?: number;
-
-	public constructor(filter?: Map<string, any>, limit?: number) {
-		this.filter = filter;
-		this.limit = limit;
-	}
-}
