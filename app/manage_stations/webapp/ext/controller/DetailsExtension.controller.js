0fa33a7691898efced70ba36c24dc38e925d8816
mmauriseidor <mmauri@seidor.es>
2021-07-15 13:19:31 +0200

    setPersistTechnicalMessages added, works fine

---
+++ app/manage_stations/webapp/ext/controller/DetailsExtension.controller.js
@@ -0,0 +1,14 @@
+sap.ui.controller("com.seidor.sfc.apps.manage.managestations.ext.controller.DetailsExtension", {
+    onInit: function (oEvent) {
+        //debugger        
+    },
+
+    onBeforeRendering: function () {
+        debugger
+        this.getView().getModel().setPersistTechnicalMessages(true)
+    },
+
+    onAfterRendering: function () {
+        //debugger
+    }
+})
---
+++ app/manage_stations/webapp/ext/controller/ListReportExtension.controller.js
@@ -0,0 +1,6 @@
+sap.ui.controller("com.seidor.sfc.apps.manage.managestations.ext.controller.ListReportExtension", {
+/*     onInit: function (oEvent) {
+        debugger
+        this.getView().getModel().setPersistTechnicalMessages(true)
+    } */
+})
